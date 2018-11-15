ab4be5202f64265f58c4921218c0c56077e547da
suveng <suveng@163.com>
2018-11-15 14:09:06 +0800

    暂存

---
+++ src/main/java/com/TT/demo/config/SessionConfig.java
@@ -0,0 +1,10 @@
+package com.TT.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@Configuration
+@EnableRedisHttpSession
+public class SessionConfig {
+
+}
--- src/main/java/com/TT/demo/controller/HelloWorldController.java
+++ src/main/java/com/TT/demo/controller/HelloWorldController.java
@@ -3,6 +3,10 @@ package com.TT.demo.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+import java.util.UUID;
+
 @Slf4j
 @RestController
 public class HelloWorldController {
@@ -12,6 +16,17 @@ public class HelloWorldController {
         log.error("测试出错===========");
         log.warn("测试警告========");
         log.info("测试信息=======");
-        return "Hello World,suveng";
+        return "Hello World,sgsdfg";
+    }
+    @RequestMapping("/uid")
+    public String uid(HttpSession session){
+        UUID uid=(UUID) session.getAttribute("uid");
+        if (null==uid){
+            uid=UUID.randomUUID();
+            log.warn("uid 成功，sessionid为"+session.getId());
+        }
+        log.warn("uid 失败，sessionid为"+session.getId()+"开始设置uid");
+        session.setAttribute("uid",uid);
+        return session.getId();
     }
 }
