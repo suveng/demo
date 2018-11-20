a4e091ad5a51840659231aacf9e2eb982fe9c80c
suveng <suveng@163.com>
2018-11-20 12:01:10 +0800

    发送thyleaf解析模板后的邮件

--- src/test/java/com/TT/demo/util/mail/TestMail.java
+++ src/test/java/com/TT/demo/util/mail/TestMail.java
@@ -5,6 +5,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 /**
  * Created by 苏文广 on 2018/11/19
@@ -14,6 +16,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class TestMail {
     @Autowired
     MailService mailService;
+    @Autowired
+    TemplateEngine templateEngine;
 
     /**
      * 测试发送简单邮件
@@ -52,5 +56,16 @@ public class TestMail {
 
         String imgPath = "C:\\Users\\Administrator.DESKTOP-72RDCCB\\Pictures\\wallheaven\\wallhaven-14013.jpg";
         mailService.sendInlineResourceMail("suveng@163.com", "带静态资源的邮件", content,imgPath ,rscId );
+    }
+
+    /**
+     * 测试解析模板再发送邮件
+     */
+    @Test
+    public void testTemplateMail(){
+        Context context = new Context();
+        context.setVariable("id", "1111");
+        String emailTemplate = templateEngine.process("emailTemplate.html", context);
+        mailService.sendHtmlMail("suveng@163.com", "模板", emailTemplate);
     }
 }
