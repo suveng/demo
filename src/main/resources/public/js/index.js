d58f8357139a8260c5d6185dba9d208e0c8c431c
suveng <suveng@163.com>
2018-12-19 18:00:02 +0800

    常用选项

--- src/main/resources/public/js/index.js
+++ src/main/resources/public/js/index.js
@@ -17,7 +17,17 @@ $("#t1").dataTable({
       "previous": "上一页",
       "next": "下一页",
       "last": "尾页"
-    },
-  }
-
+    }
+  },
+  processing: true,//是否显示处理状态(排序的时候，数据很多耗费时间长的话，也会显示这个)
+  lengthChange: true,//是否允许用户改变表格每页显示的记录数
+  orderMulti: true,  //启用多列排序
+  ordering: true,//使用排序
+  bStateSave: true,//记录cookie
+  paging: true,//是否分页
+  pagingType: "full_numbers",//除首页、上一页、下一页、末页四个按钮还有页数按钮
+  searching: false,//是否开始本地搜索
+  stateSave: false,//刷新时是否保存状态
+  autoWidth: true,//自动计算宽度
+  deferRender: true,//延迟渲染
 });
