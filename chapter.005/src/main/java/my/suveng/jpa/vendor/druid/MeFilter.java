bc4f396621205be1114619a190b4945ea0d1cdad
suveng <suveng@163.com>
2019-10-13 16:54:57 +0800

    自定义filter- 执行sql回调

--- chapter.005/src/main/java/my/suveng/jpa/vendor/druid/MeFilter.java
+++ chapter.005/src/main/java/my/suveng/jpa/vendor/druid/MeFilter.java
@@ -1,8 +1,14 @@
 package my.suveng.jpa.vendor.druid;
 
+import com.alibaba.druid.filter.FilterChain;
 import com.alibaba.druid.filter.FilterEventAdapter;
+import com.alibaba.druid.proxy.jdbc.ConnectionProxy;
+import com.alibaba.druid.proxy.jdbc.PreparedStatementProxy;
+import com.alibaba.druid.proxy.jdbc.ResultSetProxy;
 import com.alibaba.druid.proxy.jdbc.StatementProxy;
 import org.springframework.stereotype.Component;
+
+import java.sql.SQLException;
 
 /**
  *
@@ -11,28 +17,59 @@ import org.springframework.stereotype.Component;
  **/
 @Component(value = "me")
 public class MeFilter extends FilterEventAdapter {
-	private String hello;
-	private String user;
+
+
+	@Override
+	protected void statementExecuteUpdateBefore(StatementProxy statement, String sql) {
+		System.out.println("insert update # before #语句在这里");
+		super.statementExecuteUpdateBefore(statement, sql);
+	}
+
+	@Override
+	protected void statementExecuteUpdateAfter(StatementProxy statement, String sql, int updateCount) {
+		System.out.println("insert update# after # 语句在这里");
+		super.statementExecuteUpdateAfter(statement, sql, updateCount);
+	}
+
+	@Override
+	protected void statementExecuteQueryBefore(StatementProxy statement, String sql) {
+		System.out.println("select # before # 语句在这里");
+		super.statementExecuteQueryBefore(statement, sql);
+	}
+
+	@Override
+	protected void statementExecuteQueryAfter(StatementProxy statement, String sql, ResultSetProxy resultSet) {
+		System.out.println("select # after # 语句在这里");
+		super.statementExecuteQueryAfter(statement, sql, resultSet);
+	}
+
+	@Override
+	protected void statementExecuteBefore(StatementProxy statement, String sql) {
+		System.out.println("DDL # before # 语句在这里");
+		super.statementExecuteBefore(statement, sql);
+	}
 
 	@Override
 	protected void statementExecuteAfter(StatementProxy statement, String sql, boolean result) {
-		System.out.println("###############################suveng#######################################"+this.hello+this.user);
+		System.out.println("DDL # after #  语句在这里");
 		super.statementExecuteAfter(statement, sql, result);
 	}
 
-	public String getHello() {
-		return hello;
+	@Override
+	protected void statementExecuteBatchBefore(StatementProxy statement) {
+		System.out.println("批处理 # before");
+		super.statementExecuteBatchBefore(statement);
 	}
 
-	public void setHello(String hello) {
-		this.hello = hello;
+	@Override
+	protected void statementExecuteBatchAfter(StatementProxy statement, int[] result) {
+		System.out.println("批处理 # after ");
+		super.statementExecuteBatchAfter(statement, result);
 	}
 
-	public String getUser() {
-		return user;
-	}
-
-	public void setUser(String user) {
-		this.user = user;
+	@Override
+	protected void statement_executeErrorAfter(StatementProxy statement, String sql, Throwable error) {
+		System.out.println("错误# after ");
+		super.statement_executeErrorAfter(statement, sql, error);
 	}
 }
