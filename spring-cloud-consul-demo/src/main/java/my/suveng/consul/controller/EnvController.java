85fe627dcfe6db1a6371af1cfcd289f363676988
suveng <suveng@163.com>
2019-12-18 09:29:24 +0800

    consul注册中心和consul配置中心完成

---
+++ spring-cloud-consul-demo/src/main/java/my/suveng/consul/controller/EnvController.java
@@ -0,0 +1,25 @@
+package my.suveng.consul.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author suwenguang
+ * @version 1.0.0
+ **/
+@RestController
+public class EnvController {
+
+	@Value("${env.version}")
+	private String version;
+
+	@Value("${env.author}")
+	private String author;
+
+	@RequestMapping("/get")
+	public String get(){
+		return version + ":" + author;
+	}
+}
