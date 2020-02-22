9a579d2e74d79d86da089381fc0efbc057899619
suveng <suveng@163.com>
2020-02-22 14:00:24 +0800

    增加jdk.18学习

---
+++ 10.guava学习/src/test/java/my/suveng/guava/controller/Random.java
@@ -0,0 +1,12 @@
+package my.suveng.guava.controller;
+
+public class Random {
+	public static void main(String[] args) {
+		java.util.Random random = new java.util.Random();
+		int i = random.nextInt();
+		int j = -3;
+		int i1 = j >>> 31;
+		System.out.println(i1);
+
+	}
+}
