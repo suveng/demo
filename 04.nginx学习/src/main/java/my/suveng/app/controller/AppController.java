3f7657ce20d6a1987e572a258dead7c876b7d169
suveng <suveng@163.com>
2020-02-24 12:43:14 +0800

    修改

--- 04.nginx学习/src/main/java/my/suveng/app/controller/AppController.java
+++ 04.nginx学习/src/main/java/my/suveng/app/controller/AppController.java
@@ -1,8 +1,5 @@
 package my.suveng.app.controller;
 
-import com.qw.support.currency.result.CodeEnum;
-import com.qw.support.currency.result.Result;
-import com.qw.support.currency.result.ResultBuilder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,10 +14,10 @@ import java.util.HashMap;
 @RestController
 public class AppController {
 	@RequestMapping("/hello")
-	public Result hello(HttpServletRequest request){
+	public Object hello(HttpServletRequest request) {
 		HashMap<String, String> stringStringHashMap = new HashMap<>();
 		stringStringHashMap.put("ip",request.getLocalAddr());
 		stringStringHashMap.put("port", String.valueOf(request.getLocalPort()));
-		return ResultBuilder.build(CodeEnum.SUCCESS,stringStringHashMap);
+		return null;
 	}
 }
