2232d6967a024eda8a05bb274047c1a14a7978e7
suveng <suveng@163.com>
2020-06-24 12:32:37 +0800

    add some

---
+++ 34.dubbo-demo/src/test/java/my/suveng/dubbodemo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package my.suveng.dubbodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
