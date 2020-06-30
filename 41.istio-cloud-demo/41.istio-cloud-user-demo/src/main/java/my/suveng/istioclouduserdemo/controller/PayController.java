22b2b06588892174424fe4d8cfd6be93b5a7f0e7
suveng <suveng@163.com>
2020-06-30 13:42:50 +0800

    grpc 微服务

---
+++ 41.istio-cloud-demo/41.istio-cloud-user-demo/src/main/java/my/suveng/istioclouduserdemo/controller/PayController.java
@@ -0,0 +1,38 @@
+package my.suveng.istioclouduserdemo.controller;
+
+import my.suveng.istio.grpc.api.order.OrderRequest;
+import my.suveng.istio.grpc.api.order.OrderResponse;
+import my.suveng.istio.grpc.api.order.OrderServiceGrpc;
+import my.suveng.istio.grpc.api.pay.PayRequest;
+import my.suveng.istio.grpc.api.pay.PayResponse;
+import my.suveng.istio.grpc.api.pay.PayServiceGrpc;
+import my.suveng.model.common.interfaces.response.IMessage;
+import my.suveng.model.common.response.Message;
+import my.suveng.util.json.Jackson;
+import net.devh.boot.grpc.client.inject.GrpcClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author suwenguang
+ **/
+@RestController
+public class PayController {
+
+	@GrpcClient("pay")
+	private PayServiceGrpc.PayServiceBlockingStub payServiceBlockingStub;
+
+	@GrpcClient("order")
+	private OrderServiceGrpc.OrderServiceBlockingStub orderServiceBlockingStub;
+
+	@GetMapping("/pay")
+	public IMessage<String> pay() {
+		OrderResponse orderResult = orderServiceBlockingStub.getRealNameByUsername(OrderRequest.newBuilder().setPay("true").build());
+		System.out.println(Jackson.toJsonString(orderResult));
+		PayResponse result = payServiceBlockingStub.getRealNameByUsername(PayRequest.newBuilder().setMoney("123").build());
+		return Message.successWithData(result.toString());
+	}
+
+
+}
---
+++ 41.istio-cloud-demo/README.md
@@ -0,0 +1,4 @@
+大概的调用逻辑
+
+user 调用pay
+user 调用order
