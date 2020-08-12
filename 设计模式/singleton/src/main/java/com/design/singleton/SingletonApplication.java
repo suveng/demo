8104aa0962a3091d9e711646736a21610d5d8930
Angbeats <yangqisen@52tt.com>
2020-08-12 17:32:42 +0800

    单例模式

---
+++ 设计模式/singleton/src/main/java/com/design/singleton/SingletonApplication.java
@@ -0,0 +1,13 @@
+package com.design.singleton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingletonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SingletonApplication.class, args);
+    }
+
+}
