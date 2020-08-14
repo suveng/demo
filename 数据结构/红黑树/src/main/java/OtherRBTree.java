19ab13145037a4973f125cb0ca56337126eedde9
suveng <suveng@163.com>
2020-08-14 19:00:59 +0800

    flink simple demo

---
+++ 数据结构/红黑树/src/main/java/OtherRBTree.java
@@ -0,0 +1,12 @@
+import sun.jvm.hotspot.utilities.RBTree;
+
+/**
+ * 其他人的红黑树 debug
+ * @author suwenguang
+ **/
+public class OtherRBTree {
+	public static void main(String[] args) {
+		// jvm 提供的红黑树测试类
+		RBTree.main(args);
+	}
+}
